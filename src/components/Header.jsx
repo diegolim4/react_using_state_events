317edc29d3002a8fcdce404e5281bf774c3a6f66
Diego <lima.j.diego@gmail.com>
2021-07-15 12:50:16 -0300

    Criado component header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <h2>Goku Virando super-sayajin</h2>
+        </header>
+    )
+}
